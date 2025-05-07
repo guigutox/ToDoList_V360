3aba0bddd38f2639cdd369876fb43d77d95a6bd3
Guilherme <guilhermegutofer@gmail.com>
2025-05-07 12:58:36 -0300

    Se aplicado, esse commit ira adicionar a controller de todo_itens juntamente com o ajuste na model para se adequar a estrutura do projeto

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,48 @@
+class TodoItemsController < ApplicationController
+    before_action :set_todo_list
+    before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+
+    def new 
+        @todo_item = @todo_list.todo_items.build
+    end
+
+    def create
+        @todo_item = @todo_list.todo_items.build(todo_item_params)
+
+        if @todo_item.save
+            redirect_to @todo_list, notice: 'Todo item was successfully created.'
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+
+    def update
+        if @todo_item.update(todo_item_params)
+            redirect_to @todo_list, notice: 'Todo item was successfully updated.'
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @todo_item.destroy
+        redirect_to @todo_list, notice: 'Todo item was successfully destroyed.'
+    end
+
+    def set_todo_list
+        @todo_list = TodoList.find(params[:todo_list_id])
+      end
+    
+    def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+    
+    def todo_item_params
+        params.require(:todo_item).permit(:content, :done)
+    end
+
+end
