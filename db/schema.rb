c2f8843fc21b89b2e6541164c20328923b3a7f31
Guilherme <guilhermegutofer@gmail.com>
2025-05-13 18:03:26 -0300

    Se aplicado, esse commit possibilita a aplicacao a utilizar postgress ao inves de sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_11_193703) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_09_121207) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
     t.boolean "done"
-    t.integer "todo_list_id", null: false
+    t.bigint "todo_list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "priority"
@@ -26,7 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_11_193703) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_todo_lists_on_user_id"
   end
 
